4fd20b78d995568704f465e02e3ca06129921bb9
patwilde <166457766+patwilde@users.noreply.github.com>
2026-05-22 13:19:40 +1000

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 99.9
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'Alice'
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
